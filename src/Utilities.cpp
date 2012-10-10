8f5f5cd965b9eea3eb3e02e358918b9dad0cdaa4
Dmitri Shuralyov <shurcooL@gmail.com>
2012-10-10 18:26:01 -0400

    Made FromFileToString() return empty string instead of throwing exception on error.

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -2,14 +2,21 @@
 
 std::string FromFileToString(std::string Path)
 {
-	std::ifstream t(Path);
-	std::string str;
+	try
+	{
+		std::ifstream t(Path);
+		std::string str;
 
-	t.seekg(0, std::ios::end);
-	str.reserve(t.tellg());
-	t.seekg(0, std::ios::beg);
+		t.seekg(0, std::ios::end);
+		str.reserve(t.tellg());
+		t.seekg(0, std::ios::beg);
 
-	str.assign((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+		str.assign((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
 
-	return str;
+		return str;
+	}
+	catch (...)
+	{
+		return "";
+	}
 }
