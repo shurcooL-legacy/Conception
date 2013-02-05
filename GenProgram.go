2b23b227a3249b56d914916298cba9ac6e7ec23d
Dmitri Shuralyov <shurcooL@gmail.com>
2013-02-04 21:35:17 -0500

    Minor.
    Set ConceptionApp as active app.

--- GenProgram.go
+++ GenProgram.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"time"
-	"net/http"
-)
-
-var _ = fmt.Printf
-var _ = time.Sleep
-var _ = http.ListenAndServe
-
 func main() {
-	println("Hi, local live editor.")
-	println()
-	print("You can do anything here.")
+	print("Hi.")
 }
