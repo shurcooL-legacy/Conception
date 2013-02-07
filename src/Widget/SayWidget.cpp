ebf0ccdc8fff3e4c440238addb07a818404c926e
Dmitri Shuralyov <shurcooL@gmail.com>
2013-02-07 15:56:32 -0500

    Re-added button to SayWidget.

--- src/Widget/SayWidget.cpp
+++ src/Widget/SayWidget.cpp
@@ -2,15 +2,13 @@
 
 SayWidget::SayWidget(Vector2n Position, TypingModule & TypingModule)
 	: FlowLayoutWidget(Position, { std::shared_ptr<Widget>(m_InputWidget = new TextFieldWidget(Vector2n::ZERO, TypingModule)),
-								   std::shared_ptr<Widget>(new LabelWidget(Vector2n::ZERO, std::string("say"), LabelWidget::Background::Normal))/*,
-								   std::shared_ptr<Widget>(m_ExecuteWidget = new ButtonWidget(Vector2n::ZERO, Vector2n(16, 16), [&](){} ))*/ }, { std::shared_ptr<Behavior>(new DraggablePositionBehavior(*this)) })
+								   std::shared_ptr<Widget>(new LabelWidget(Vector2n::ZERO, std::string("say"), LabelWidget::Background::Normal)),
+								   std::shared_ptr<Widget>(m_ExecuteWidget = new ButtonWidget(Vector2n::ZERO, Vector2n(16, 16), [&](){} )) }, { std::shared_ptr<Behavior>(new DraggablePositionBehavior(*this)) })
 {
 	// TEST
 	m_InputWidget->SetContent("Shall we play a game?");
 
 	{
-		m_ExecuteWidget = new ButtonWidget(Vector2n::ZERO, Vector2n(16, 16), [](){});
-
 		m_ExecuteWidget->SetAction([&]()
 		{
 			{
