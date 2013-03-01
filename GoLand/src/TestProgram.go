0000b519549e8a09e14a50ccf0164d905e1862cd
Dmitri Shuralyov <shurcooL@gmail.com>
2013-02-28 21:44:42 -0500

    Minor.
    Consolidated GoLand.

--- GoLand/src/TestProgram.go
+++ GoLand/src/TestProgram.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"gist.github.com/4668739.git"
-	"gist.github.com/4670289.git"
-	. "gist.github.com/4727543.git"
-)
-
-var _ = gist4668739.HttpGet
-var _ = gist4670289.GoKeywords
+import . "gist.github.com/5022726.git"
 
 func main() {
-	println("Hi.")
-	println(GetForcedUse("gist.github.com/4670289.git"))
-	println(GetForcedUseRenamed("gist.github.com/4670289.git", ""))
-	println(GetForcedUseRenamed("gist.github.com/4670289.git", "NewPkgName"))
-	println(GetForcedUseRenamed("gist.github.com/4670289.git", "."))
+	print("$" + GetWeeklyIncome("shurcooL"))
 }
