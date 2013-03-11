28f3faa5b2fb7b5302e999b6b014105e8ae2c541
Dmitri Shuralyov <shurcooL@gmail.com>
2013-03-11 14:01:34 -0400

    Check if TextFileWidget file has been modified externally, update it if so.

--- src/Widget/TextFileWidget.cpp
+++ src/Widget/TextFileWidget.cpp
@@ -3,7 +3,8 @@
 TextFileWidget::TextFileWidget(Vector2n Position, std::string Path, TypingModule & TypingModule)
 	: CompositeWidget(Position, { std::shared_ptr<Widget>(new LabelWidget(Vector2n(0, -lineHeight - 2), Path, LabelWidget::Background::Normal)),
 								  std::shared_ptr<Widget>(m_TextFieldWidget = new TextFieldWidget(Vector2n::ZERO, TypingModule)) },
-								{ std::shared_ptr<Behavior>(new DraggablePositionBehavior(*this)) })
+								{ std::shared_ptr<Behavior>(new DraggablePositionBehavior(*this)) }),
+	  m_Path(Path)
 {
 	m_TextFieldWidget->SetContent(FromFileToString(Path));
 	m_TextFieldWidget->m_OnChange = [=]()
@@ -22,4 +23,21 @@ TextFileWidget::TextFileWidget(Vector2n Position, std::string Path, TypingModule
 
 TextFileWidget::~TextFileWidget()
 {
+}
+
+void TextFileWidget::ProcessTimePassed(const double TimePassed)
+{
+	// Check if the file has been changed externally, and if so, override this widget
+	{
+		auto NewContent = FromFileToString(m_Path);
+		if (NewContent != m_TextFieldWidget->GetContent())
+			m_TextFieldWidget->SetContent(NewContent);
+	}
+
+	CompositeWidget::ProcessTimePassed(TimePassed);
+}
+
+std::string TextFileWidget::GetPath() const
+{
+	return m_Path;
 }
--- src/Widget/TextFileWidget.h
+++ src/Widget/TextFileWidget.h
@@ -9,6 +9,10 @@ public:
 	TextFileWidget(Vector2n Position, std::string Path, TypingModule & TypingModule);
 	~TextFileWidget();
 
+	void ProcessTimePassed(const double TimePassed) override;
+
+	std::string GetPath() const;
+
 	std::function<void()> m_OnChange = nullptr;
 
 private:
@@ -17,6 +21,8 @@ private:
 
 	// HACK, TODO: Keep this private by generalizing some interface so ConnectionWidget<TextFieldWidget> can accept TextFileWidget too
 	public:TextFieldWidget * m_TextFieldWidget;private:
+
+	std::string m_Path;
 };
 
 #endif // __TextFileWidget_H__
